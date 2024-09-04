4d073d7d7e68c3a665c443c6a314978b4eb731bb
chenxi <chen_xi@torihada.co.jp>
2024-09-04 10:27:53 +0900

    css

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const lilita = Lilita_One({ weight: "400", subsets: ["latin"] });
 const Header = async () => {
   const user = await getUser();
   return (
-    <div className="relative mt-2 flex h-20 w-full max-w-6xl items-center justify-between rounded-lg bg-popover px-4">
+    <div className="relative m-auto mt-2 flex h-20 w-full max-w-6xl items-center justify-between rounded-lg bg-popover px-4">
       <UserButton user={user} />
       <h1
         className={`${lilita.className} text-4xl text-popover-foreground sm:text-5xl`}
